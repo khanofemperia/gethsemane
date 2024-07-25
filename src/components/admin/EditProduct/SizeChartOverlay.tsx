a0e1d144e54486dbc7d47b8b3ba8a93f1071bd27
Bundlestand <141491831+Bundlestand-v1@users.noreply.github.com>
2024-07-25 07:37:50 +0000

    commit

--- src/components/admin/EditProduct/SizeChartOverlay.tsx
+++ src/components/admin/EditProduct/SizeChartOverlay.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useOverlayStore } from "@/zustand/admin/overlayStore";
-import { EditIcon } from "@/icons";
+import { CloseIcon, EditIcon } from "@/icons";
 import Overlay from "@/ui/Overlay";
 import { useState, useEffect, useCallback } from "react";
 import { HiPlus, HiMinus } from "react-icons/hi";
@@ -240,24 +240,131 @@ export function SizeChartOverlay({ data }: { data: DataType }) {
     <>
       {isOverlayVisible && (
         <Overlay>
-          <div className="absolute bottom-0 left-0 right-0 w-full h-[calc(100%-60px)] rounded-t-3xl overflow-hidden bg-white md:w-max md:max-w-[740px] md:min-w-[516px] md:rounded-2xl md:shadow md:h-max md:mx-auto md:mt-20 md:mb-[50vh] md:relative md:bottom-auto md:left-auto md:right-auto md:top-auto md:-translate-x-0">
-            <div className="hidden md:flex md:items-center md:justify-between py-2 pr-4 pl-2">
-              <button
-                onClick={() => {
-                  hideOverlay({ pageName, overlayName });
-                }}
-                type="button"
-                className="h-9 px-3 rounded-full flex items-center gap-1 transition duration-300 ease-in-out active:bg-lightgray"
-              >
-                <ArrowLeftIcon className="fill-blue -ml-[2px]" size={20} />
-                <span className="font-semibold text-sm text-blue">Sizes</span>
-              </button>
+          <div className="absolute bottom-0 left-0 right-0 w-full h-[calc(100%-60px)] rounded-t-3xl overflow-hidden bg-white md:w-max md:min-w-[516px] md:max-w-[740px] md:rounded-2xl md:shadow md:h-max md:mx-auto md:mt-20 md:mb-[50vh] md:relative md:bottom-auto md:left-auto md:right-auto md:top-auto md:-translate-x-0">
+            <div className="w-full h-[calc(100vh-188px)] md:h-auto">
+              <div className="md:hidden flex items-end justify-center pt-4 pb-2 absolute top-0 left-0 right-0 bg-white">
+                <div className="relative flex justify-center items-center w-full h-7">
+                  <h2 className="font-semibold text-lg">Sizes</h2>
+                  <button
+                    onClick={() => {
+                      hideOverlay({ pageName, overlayName });
+                    }}
+                    type="button"
+                    className="w-7 h-7 rounded-full flex items-center justify-center absolute right-4 transition duration-300 ease-in-out bg-lightgray active:bg-lightgray-dimmed"
+                  >
+                    <CloseIcon size={18} />
+                  </button>
+                </div>
+              </div>
+              <div className="hidden md:flex md:items-center md:justify-between py-2 pr-4 pl-2">
+                <button
+                  onClick={() => {
+                    hideOverlay({ pageName, overlayName });
+                  }}
+                  type="button"
+                  className="h-9 px-3 rounded-full flex items-center gap-1 transition duration-300 ease-in-out active:bg-lightgray"
+                >
+                  <ArrowLeftIcon className="fill-blue -ml-[2px]" size={20} />
+                  <span className="font-semibold text-sm text-blue">Sizes</span>
+                </button>
+                <button
+                  onClick={handleSave}
+                  type="button"
+                  disabled={loading}
+                  className={clsx(
+                    "relative h-9 w-max px-4 rounded-full overflow-hidden transition duration-300 ease-in-out text-white bg-neutral-700",
+                    {
+                      "bg-opacity-50": loading,
+                      "active:bg-neutral-700": !loading,
+                    }
+                  )}
+                >
+                  {loading ? (
+                    <div className="flex gap-1 items-center justify-center w-full h-full">
+                      <Spinner />
+                      <span className="text-white">Saving</span>
+                    </div>
+                  ) : (
+                    <span className="text-white">Save</span>
+                  )}
+                </button>
+              </div>
+              <div className="w-full h-full mt-[52px] md:mt-0 p-5 pb-28 md:pb-10 flex flex-col gap-5 overflow-x-hidden overflow-y-visible invisible-scrollbar md:overflow-hidden">
+                <div className="mb-5 flex gap-5">
+                  <div className="flex items-center gap-2">
+                    <span className="font-semibold text-sm">Rows</span>
+                    <div className="w-max flex border rounded-full overflow-hidden">
+                      <button
+                        type="button"
+                        onClick={removeRow}
+                        className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
+                      >
+                        <HiMinus className="fill-stone-500" size={20} />
+                      </button>
+                      <div className="w-[1px] bg-[#dcdfe4]"></div>
+                      <button
+                        type="button"
+                        onClick={addRow}
+                        className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
+                      >
+                        <HiPlus className="fill-gray" size={18} />
+                      </button>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <span className="font-semibold text-sm">Columns</span>
+                    <div className="w-max flex border rounded-full overflow-hidden">
+                      <button
+                        type="button"
+                        onClick={removeColumn}
+                        className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
+                      >
+                        <HiMinus className="fill-gray" size={20} />
+                      </button>
+                      <div className="w-[1px] bg-[#dcdfe4]"></div>
+                      <button
+                        type="button"
+                        onClick={addColumn}
+                        className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
+                      >
+                        <HiPlus className="fill-gray" size={18} />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                {tableData.inches.rows.length > 0 ? (
+                  <>
+                    <div className="mb-5">
+                      <h2 className="text-xs text-gray mb-2">Inches</h2>
+                      <SizesTable
+                        data={tableData.inches.rows}
+                        columns={columns}
+                        onUpdate={updateInchesData}
+                        onColumnUpdate={updateColumns}
+                      />
+                    </div>
+                    <div className="mb-5">
+                      <h2 className="text-xs text-gray mb-2">Centimeters</h2>
+                      <SizesTable
+                        data={tableData.centimeters.rows}
+                        columns={columns}
+                        onUpdate={updateCentimetersData}
+                        onColumnUpdate={updateColumns}
+                      />
+                    </div>
+                  </>
+                ) : (
+                  <p>No data available. Add a row to start.</p>
+                )}
+              </div>
+            </div>
+            <div className="md:hidden w-full pb-5 pt-2 px-5 absolute bottom-0">
               <button
                 onClick={handleSave}
                 type="button"
                 disabled={loading}
                 className={clsx(
-                  "relative h-9 w-max px-4 rounded-full overflow-hidden transition duration-300 ease-in-out text-white bg-neutral-700",
+                  "relative h-12 w-full rounded-full overflow-hidden transition duration-300 ease-in-out text-white bg-neutral-700",
                   {
                     "bg-opacity-50": loading,
                     "active:bg-neutral-700/85": !loading,
@@ -273,78 +380,6 @@ export function SizeChartOverlay({ data }: { data: DataType }) {
                   <span className="text-white">Save</span>
                 )}
               </button>
-            </div>
-            <div className="p-5">
-              <div className="mb-5 flex gap-5">
-                <div className="flex items-center gap-2">
-                  <span className="font-semibold text-sm">Rows</span>
-                  <div className="w-max flex border rounded-full overflow-hidden">
-                    <button
-                      type="button"
-                      onClick={removeRow}
-                      className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
-                    >
-                      <HiMinus className="fill-stone-500" size={20} />
-                    </button>
-                    <div className="w-[1px] bg-[#dcdfe4]"></div>
-                    <button
-                      type="button"
-                      onClick={addRow}
-                      className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
-                    >
-                      <HiPlus className="fill-gray" size={18} />
-                    </button>
-                  </div>
-                </div>
-                <div className="flex items-center gap-2">
-                  <span className="font-semibold text-sm">Columns</span>
-                  <div className="w-max flex border rounded-full overflow-hidden">
-                    <button
-                      type="button"
-                      onClick={removeColumn}
-                      className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
-                    >
-                      <HiMinus className="fill-gray" size={20} />
-                    </button>
-                    <div className="w-[1px] bg-[#dcdfe4]"></div>
-                    <button
-                      type="button"
-                      onClick={addColumn}
-                      className="h-6 w-9 grid place-items-center ease-in-out duration-300 transition hover:bg-neutral-200"
-                    >
-                      <HiPlus className="fill-gray" size={18} />
-                    </button>
-                  </div>
-                </div>
-              </div>
-              {tableData.inches.rows.length > 0 ? (
-                <>
-                  <div className="mb-5">
-                    <h2 className="text-xs text-gray mb-2">
-                      Inches
-                    </h2>
-                    <SizesTable
-                      data={tableData.inches.rows}
-                      columns={columns}
-                      onUpdate={updateInchesData}
-                      onColumnUpdate={updateColumns}
-                    />
-                  </div>
-                  <div className="mb-5">
-                    <h2 className="text-xs text-gray mb-2">
-                      Centimeters
-                    </h2>
-                    <SizesTable
-                      data={tableData.centimeters.rows}
-                      columns={columns}
-                      onUpdate={updateCentimetersData}
-                      onColumnUpdate={updateColumns}
-                    />
-                  </div>
-                </>
-              ) : (
-                <p>No data available. Add a row to start.</p>
-              )}
             </div>
           </div>
         </Overlay>
