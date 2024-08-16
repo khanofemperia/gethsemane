12411c5f4f880037dad61d5b81edb2ea47b820c2
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-08-16 07:16:50 +0000

    commit

--- src/components/website/UpsellReviewOverlay.tsx
+++ src/components/website/UpsellReviewOverlay.tsx
@@ -42,50 +42,51 @@ export function UpsellReviewButton() {
 export function UpsellReviewOverlay() {
   return (
     <div className="custom-scrollbar flex justify-center py-20 w-screen h-screen overflow-x-hidden overflow-y-visible z-20 fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-40 backdrop-blur-sm">
-      <div className="w-[800px] p-10 absolute top-16 bottom-16 bg-white mx-auto shadow rounded-2xl">
-        <div className="flex flex-row gap-8 custom-scrollbar max-h-[554px] h-full overflow-x-hidden overflow-y-visible">
-          <div className="w-full">
-            <div className="w-max max-w-[294px] h-8 mx-auto flex flex-wrap items-center justify-center gap-y-2 gap-x-4">
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                1
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                2
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                3
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                4
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                5
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                6
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                7
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                8
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                9
-              </div>
-              <div className="w-7 h-7 rounded-full text-sm flex items-center justify-center bg-lightgray">
-                10
-              </div>
-            </div>
+      <div className="relative overflow-hidden shadow rounded-2xl bg-white">
+        <div className="w-[640px] h-full pt-[40px] pb-[85px] flex flex-col relative">
+          <div className="w-max mx-auto flex gap-y-1 gap-x-2 *:w-7 *:h-7 *:rounded-full *:bg-lightgray *:flex *:items-center *:justify-center *:text-xs">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+          </div>
+          <div className="custom-scrollbar overflow-x-hidden overflow-y-visible">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+            recusandae amet rem sint aliquid quod architecto quas iure, tenetur
+            enim, aperiam cupiditate. Possimus iusto et commodi facere alias
+            eius deleniti. Enim maxime architecto asperiores sit illum cum
+            provident debitis nesciunt, corporis totam quis omnis, inventore
+            expedita error, eum voluptatem quibusdam eveniet? Molestiae quis
+            laboriosam quaerat eos cupiditate atque, nemo totam? Possimus
+            repellendus maiores repudiandae et quidem, quas tempore aspernatur
+            eligendi facere voluptate sequi deleniti animi harum eum eius
+            quisquam hic iusto odio qui voluptas ducimus! Nobis aliquam
+            inventore consequuntur corrupti! A neque quia vero voluptas,
+            nesciunt culpa facilis dolorem est accusantium, ipsum vel asperiores
+            fugiat molestias, praesentium repellendus rem nostrum magni enim.
+            Aliquid quidem laudantium expedita ipsa. Dicta, labore excepturi!
+            Esse mollitia perspiciatis officiis blanditiis nesciunt natus.
+            Voluptas quae quis ipsam suscipit laboriosam porro eos numquam
+            temporibus repellendus, molestias qui nihil expedita nisi maiores
+            enim dignissimos ducimus perspiciatis eveniet! Est? Beatae iure
+            commodi consectetur possimus nobis ullam ad inventore ipsum
+            provident, ab fuga repudiandae accusantium nesciunt illum quasi
+            quibusdam numquam sit quidem. Quisquam unde repudiandae, quibusdam
+            vitae tenetur repellendus doloremque? Fugiat labore ipsum ut id
+            cupiditate exercitationem, nam alias vitae illo repellendus
+            perspiciatis sequi ducimus? Magnam laborum sit similique! Veritatis,
+            corrupti? Excepturi officiis impedit aspernatur quae incidunt
+            aliquid ipsam esse! Nihil iure animi repellat cumque tempora eaque,
+            voluptatum autem illo. Aliquid dolorum laboriosam deserunt quia
+            animi hic, corrupti natus modi dolore! Ea suscipit magni alias
+            dolore optio voluptatibus ratione minima!
+          </div>
+          <div className="absolute left-0 right-0 bottom-0">
+            <div className="h-[85px] bg-slate-300"></div>
           </div>
         </div>
-        <button
-          // onClick={hideOverlay}
-          className="h-8 w-8 rounded-full absolute right-2 top-2 flex items-center justify-center"
-          type="button"
-        >
-          <CloseIcon size={18} className="fill-gray" />
-        </button>
+        <button className="w-9 h-9 rounded-full absolute top-2 right-2 flex items-center justify-center ease-in-out transition duration-300 hover:bg-lightgray"></button>
       </div>
     </div>
   );
