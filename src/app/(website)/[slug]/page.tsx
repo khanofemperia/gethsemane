6d196fda08b4ee6b6fb1a24d73fae2c5ef25b80b
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-08-18 12:52:13 +0000

    commit

--- src/app/(website)/[slug]/page.tsx
+++ src/app/(website)/[slug]/page.tsx
@@ -351,7 +351,7 @@ export default async function ProductDetails({
                 <button className="leading-5 text-[13px] min-[340px]:text-sm font-semibold w-full h-[44px] rounded-full ease-in-out duration-150 transition border border-[rgb(150,150,150)] hover:border-[rgb(80,80,80)] active:border-[rgb(150,150,150)] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.16)]">
                   Add to Cart
                 </button>
-                <button className="leading-5 text-[13px] min-[340px]:text-sm inline-block text-center align-middle h-[44px] w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-300 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
+                <button className="leading-5 text-[13px] min-[340px]:text-sm inline-block text-center align-middle h-[44px] w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
                   Yes, Let's Upgrade
                 </button>
               </div>
@@ -501,7 +501,7 @@ export default async function ProductDetails({
                     <button className="text-sm min-[896px]:text-base font-semibold w-full h-[44px] min-[896px]:h-12  rounded-full ease-in-out duration-150 transition border border-[rgb(150,150,150)] hover:border-[rgb(80,80,80)] active:border-[rgb(150,150,150)] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.16)]">
                       Add to Cart
                     </button>
-                    <button className="text-sm min-[896px]:text-base inline-block text-center align-middle h-[44px] min-[896px]:h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-300 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
+                    <button className="text-sm min-[896px]:text-base inline-block text-center align-middle h-[44px] min-[896px]:h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
                       Yes, Let's Upgrade
                     </button>
                   </div>
--- src/components/website/UpsellReviewOverlay.tsx
+++ src/components/website/UpsellReviewOverlay.tsx
@@ -2,11 +2,11 @@
 
 import Image from "next/image";
 import { useOverlayStore } from "@/zustand/website/overlayStore";
-import { useCallback, useEffect, useState } from "react";
-import { useOptionsStore } from "@/zustand/website/optionsStore";
+import { useState } from "react";
 import { ChevronRightIcon, CloseIconThin } from "@/icons";
 import { useUpsellReviewStore } from "@/zustand/website/upsellReviewStore";
 import clsx from "clsx";
+import { formatThousands } from "@/lib/utils";
 
 type UpsellReviewProductType = {
   id: string;
@@ -82,15 +82,28 @@ function ProductColors({
   );
 }
 
+type SizeChartType = {
+  inches: {
+    columns: Array<{ label: string; order: number }>;
+    rows: Array<{ [key: string]: string }>;
+  };
+  centimeters: {
+    columns: Array<{ label: string; order: number }>;
+    rows: Array<{ [key: string]: string }>;
+  };
+};
+
+type ProductSizeChartProps = {
+  sizeChart: SizeChartType;
+  selectedSize: string;
+  onSizeSelect: (size: string) => void;
+};
+
 function ProductSizeChart({
   sizeChart,
   selectedSize,
   onSizeSelect,
-}: {
-  sizeChart: SizeChartType;
-  selectedSize: string;
-  onSizeSelect: (size: string) => void;
-}) {
+}: ProductSizeChartProps) {
   const { showOverlay } = useOverlayStore();
   const { pageName, overlayName } = useOverlayStore((state) => ({
     pageName: state.pages.productDetails.name,
@@ -162,45 +175,45 @@ function ProductSizeChart({
   );
 }
 
+type ProductOptionsProps = {
+  product: {
+    options: {
+      colors: Array<{
+        name: string;
+        image: string;
+      }>;
+      sizes: SizeChartType;
+    };
+  };
+  selectedOptions: {
+    color?: string;
+    size?: string;
+  };
+  onOptionSelect: (option: string, value: string) => void;
+};
+
 function ProductOptions({
   product,
-}: {
-  product: UpsellReviewProductType["upsell"]["products"][0];
-}) {
-  const [selectedColor, setSelectedColor] = useState("");
-  const [selectedSize, setSelectedSize] = useState("");
-
+  selectedOptions,
+  onOptionSelect,
+}: ProductOptionsProps) {
   const hasColor = product.options.colors.length > 0;
   const hasSize = Object.keys(product.options.sizes).length > 0;
 
   return (
-    <div>
-      {hasColor && hasSize && (
-        <div className="flex flex-col gap-4 select-none">
-          <ProductColors
-            colors={product.options.colors}
-            selectedColor={selectedColor}
-            onColorSelect={setSelectedColor}
-          />
-          <ProductSizeChart
-            sizeChart={product.options.sizes}
-            selectedSize={selectedSize}
-            onSizeSelect={setSelectedSize}
-          />
-        </div>
-      )}
-      {hasColor && !hasSize && (
+    <div className="flex flex-col gap-4 select-none">
+      {hasColor && (
         <ProductColors
           colors={product.options.colors}
-          selectedColor={selectedColor}
-          onColorSelect={setSelectedColor}
+          selectedColor={selectedOptions.color || ""}
+          onColorSelect={(color) => onOptionSelect("color", color)}
         />
       )}
-      {!hasColor && hasSize && (
+      {hasSize && (
         <ProductSizeChart
           sizeChart={product.options.sizes}
-          selectedSize={selectedSize}
-          onSizeSelect={setSelectedSize}
+          selectedSize={selectedOptions.size || ""}
+          onSizeSelect={(size) => onOptionSelect("size", size)}
         />
       )}
     </div>
@@ -226,7 +239,7 @@ export function UpsellReviewButton({
     <button
       type="button"
       onClick={openOverlay}
-      className="text-sm min-[896px]:text-base inline-block text-center align-middle h-[44px] min-[896px]:h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-300 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]"
+      className="text-sm min-[896px]:text-base inline-block text-center align-middle h-[44px] min-[896px]:h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]"
     >
       Yes, Let's Upgrade
     </button>
@@ -235,26 +248,62 @@ export function UpsellReviewButton({
 
 export function UpsellReviewOverlay() {
   const { hideOverlay, isVisible, selectedProduct } = useUpsellReviewStore();
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: { color?: string; size?: string };
+  }>({});
+
+  const updateSelectedOptions = (
+    productId: string,
+    option: string,
+    value: string
+  ) => {
+    setSelectedOptions((prev) => ({
+      ...prev,
+      [productId]: { ...prev[productId], [option]: value },
+    }));
+  };
+
+  const isProductReady = (
+    product: UpsellReviewProductType["upsell"]["products"][number]
+  ) => {
+    const productOptions = selectedOptions[product.id] || {};
+    const hasColor = product.options.colors.length > 0;
+    const hasSize = Object.keys(product.options.sizes).length > 0;
+
+    if (!hasColor && !hasSize) return true;
+    if (hasColor && !productOptions.color) return false;
+    if (hasSize && !productOptions.size) return false;
+    return true;
+  };
 
   return (
     isVisible &&
     selectedProduct && (
       <div className="custom-scrollbar flex justify-center py-20 w-screen h-screen overflow-x-hidden overflow-y-visible z-30 fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-40 backdrop-blur-sm">
         <div className="max-h-[764px] relative overflow-hidden shadow rounded-2xl bg-white">
-          <div className="w-[764px] h-full pt-8 pb-[85px] flex flex-col relative">
+          <div className="w-max h-full pt-8 pb-[80px] flex flex-col relative">
             <div className="mb-2">
-              <div className="w-max mx-auto flex gap-y-1 gap-x-3 *:w-7 *:h-7 *:rounded-full *:bg-lightgray *:flex *:items-center *:justify-center *:text-xs">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
+              <div className="w-max mx-auto flex gap-y-1 gap-x-3">
+                {selectedProduct.upsell.products.map((product, index) => (
+                  <div
+                    key={product.id}
+                    className={clsx(
+                      "w-7 h-7 rounded-full flex items-center justify-center text-xs",
+                      {
+                        "bg-black text-white": isProductReady(product),
+                        "bg-lightgray text-black": !isProductReady(product),
+                      }
+                    )}
+                  >
+                    {index + 1}
+                  </div>
+                ))}
               </div>
             </div>
-            <div className="px-8 pt-3 flex flex-col gap-2 items-center custom-scrollbar overflow-x-hidden overflow-y-visible">
+            <div className="px-8 pt-2 pb-5 flex flex-col gap-2 items-center custom-scrollbar overflow-x-hidden overflow-y-visible">
               {selectedProduct.upsell.products.map((product, index) => (
                 <div
-                  key={index}
+                  key={product.id}
                   className="w-[522px] flex gap-5 border p-2 pr-5 rounded-2xl"
                 >
                   <div className="min-w-36 max-w-36 min-h-36 max-h-36 rounded-xl overflow-hidden">
@@ -268,16 +317,35 @@ export function UpsellReviewOverlay() {
                   </div>
                   <div className="mt-1 flex flex-col gap-4">
                     <p className="text-sm text-gray">{product.name}</p>
-                    <ProductOptions product={product} />
+                    <ProductOptions
+                      product={product}
+                      selectedOptions={selectedOptions[product.id] || {}}
+                      onOptionSelect={(option: string, value: string) =>
+                        updateSelectedOptions(product.id, option, value)
+                      }
+                    />
                   </div>
                 </div>
               ))}
             </div>
             <div className="absolute left-0 right-0 bottom-0">
-              <div className="h-[85px] pt-2 flex justify-center">
-                <button className="h-12 w-max px-20 inline-block text-center align-middle border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-300 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
-                  All Set! Add Upgrade to Cart
-                </button>
+              <div className="h-[80px] px-12 flex items-start shadow-[0_-12px_16px_2px_white]">
+                <div className="w-full flex justify-between items-center">
+                  <div className="mt-1 text-xl text-center font-semibold">
+                    {selectedProduct.upsell.pricing.salePrice
+                      ? `$${formatThousands(
+                          Number(selectedProduct.upsell.pricing.salePrice)
+                        )} (${
+                          selectedProduct.upsell.pricing.discountPercentage
+                        }% Off)`
+                      : `$${formatThousands(
+                          Number(selectedProduct.upsell.pricing.basePrice)
+                        )} today`}
+                  </div>
+                  <button className="h-12 w-max px-12 inline-block text-center align-middle border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
+                    All Set! Add Upgrade to Cart
+                  </button>
+                </div>
               </div>
             </div>
           </div>
