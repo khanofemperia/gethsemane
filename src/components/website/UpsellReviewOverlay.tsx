7e5cfbc6e98ef5f659fb2d554527c37a8e99f651
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-08-18 22:46:40 +0000

    commit

--- src/components/website/UpsellReviewOverlay.tsx
+++ src/components/website/UpsellReviewOverlay.tsx
@@ -347,7 +347,7 @@ export function UpsellReviewOverlay() {
                           Number(selectedProduct.upsell.pricing.basePrice)
                         )} today`}
                   </div>
-                  <button className="h-12 w-max px-8 inline-block text-center align-middle border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
+                  <button className="cursor-not-allowed h-12 w-max px-8 inline-block text-center align-middle border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
                     All Set! Add Upgrade to Cart
                   </button>
                 </div>
