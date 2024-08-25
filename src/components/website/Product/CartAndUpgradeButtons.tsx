a60e0ba682f3634cc5dc97047e24eee416b24197
khanofemperia <103168530+khanofemperia@users.noreply.github.com>
2024-08-25 21:34:07 +0000

    commit

--- src/components/website/Product/CartAndUpgradeButtons.tsx
+++ src/components/website/Product/CartAndUpgradeButtons.tsx
@@ -5,8 +5,9 @@ import { useOptionsStore } from "@/zustand/website/optionsStore";
 import { useState, useTransition, useEffect } from "react";
 import { AddToCartAction } from "@/actions/add-to-cart";
 import SpinnerGray from "@/ui/Spinners/Gray";
-import { AlertMessageType } from "@/lib/sharedTypes";
+import Link from "next/link";
 import clsx from "clsx";
+import { AlertMessageType } from "@/lib/sharedTypes";
 
 type CartAndUpgradeButtonsType = {
   productId: string;
@@ -78,7 +79,13 @@ export function CartAndUpgradeButtons({
     });
   };
 
-  return (
+  return isInCart ? (
+    <Link href="/cart" className="w-full">
+      <button className="text-sm min-[896px]:text-base inline-block text-center align-middle h-[44px] min-[896px]:h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
+        Added - View Cart
+      </button>
+    </Link>
+  ) : (
     <>
       <button
         onClick={handleAddToCart}
@@ -88,13 +95,7 @@ export function CartAndUpgradeButtons({
           { "cursor-context-menu opacity-50": isPending }
         )}
       >
-        {isPending ? (
-          <SpinnerGray size={28} />
-        ) : isInCart ? (
-          "Go to Cart"
-        ) : (
-          "Add to Cart"
-        )}
+        {isPending ? <SpinnerGray size={28} /> : "Add to Cart"}
       </button>
       <button className="text-sm min-[896px]:text-base inline-block text-center align-middle h-[44px] min-[896px]:h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
         Yes, Let's Upgrade
