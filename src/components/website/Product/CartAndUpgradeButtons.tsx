ec5ffb699c1bff85b647baf23961592810a24f33
khanofemperia <103168530+khanofemperia@users.noreply.github.com>
2024-08-29 11:09:35 +0000

    commit

--- src/components/website/Product/CartAndUpgradeButtons.tsx
+++ src/components/website/Product/CartAndUpgradeButtons.tsx
@@ -78,22 +78,20 @@ export function CartAndUpgradeButtons({
     });
   };
 
-  return isInCart ? (
-    <button className="text-sm min-[896px]:text-base inline-block text-center align-middle h-[44px] min-[896px]:h-12 w-full border border-[rgba(0,0,0,0.1)_rgba(0,0,0,0.1)_rgba(0,0,0,0.25)] rounded-full ease-in-out duration-100 transition bg-amber hover:bg-amber-dimmed active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.2)] font-semibold text-white shadow-[inset_0_1px_0_rgba(255,255,255,0.2),0_1px_2px_rgba(0,0,0,0.05)]">
-      Yes, let's upgrade
-    </button>
-  ) : (
+  return (
     <>
-      <button
-        onClick={handleAddToCart}
-        disabled={isPending}
-        className={clsx(
-          "flex items-center justify-center w-full rounded-full cursor-pointer border border-[#c5c3c0] text-sm font-semibold h-[44px] shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#faf9f8_5%,_#eae8e6_100%)] bg-[#faf9f8] hover:[background:linear-gradient(to_bottom,_#eae8e6_5%,_#faf9f8_100%)] hover:bg-[#eae8e6] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.14)] min-[896px]:text-base min-[896px]:h-12",
-          { "cursor-context-menu opacity-50": isPending }
-        )}
-      >
-        {isPending ? <SpinnerGray size={28} /> : "Add to cart"}
-      </button>
+      {!isInCart && (
+        <button
+          onClick={handleAddToCart}
+          disabled={isPending}
+          className={clsx(
+            "flex items-center justify-center w-full rounded-full cursor-pointer border border-[#c5c3c0] text-sm font-semibold h-[44px] shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#faf9f8_5%,_#eae8e6_100%)] bg-[#faf9f8] hover:[background:linear-gradient(to_bottom,_#eae8e6_5%,_#faf9f8_100%)] hover:bg-[#eae8e6] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.14)] min-[896px]:text-base min-[896px]:h-12",
+            { "cursor-context-menu opacity-50": isPending }
+          )}
+        >
+          {isPending ? <SpinnerGray size={28} /> : "Add to cart"}
+        </button>
+      )}
       <button className="flex items-center justify-center w-full rounded-full cursor-pointer border border-[#b27100] text-white text-sm font-semibold h-[44px] shadow-[inset_0px_1px_0px_0px_#ffa405] [background:linear-gradient(to_bottom,_#e29000_5%,_#cc8100_100%)] bg-[#e29000] hover:bg-[#cc8100] hover:[background:linear-gradient(to_bottom,_#cc8100_5%,_#e29000_100%)] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.14)] min-[896px]:text-base min-[896px]:h-12">
         Yes, let's upgrade
       </button>
