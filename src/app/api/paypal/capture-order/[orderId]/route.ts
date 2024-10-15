526b0636f32c89ed443fb2c2386c261114182f09
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-10-15 12:44:47 +0000

    commit

--- src/app/api/paypal/capture-order/[orderId]/route.ts
+++ src/app/api/paypal/capture-order/[orderId]/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { setDoc, doc } from "firebase/firestore";
 import { database } from "@/lib/firebase";
+import { cookies } from "next/headers";
+import { getCart, getProductsByIds } from "@/lib/getData";
+import { getDoc } from "firebase/firestore";
 
 const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
 const PAYPAL_CLIENT_SECRET = process.env.NEXT_PAYPAL_CLIENT_SECRET;
@@ -38,6 +41,7 @@ export async function POST(
     }
 
     const orderData = await response.json();
+    const cartItems = await getCartItems();
 
     const newOrder = {
       status: orderData.status,
@@ -66,6 +70,7 @@ export async function POST(
       },
       transactionId: orderData.purchase_units[0].payments.captures[0].id,
       timestamp: orderData.purchase_units[0].payments.captures[0].create_time,
+      items: cartItems,
     };
 
     const orderRef = doc(database, "orders", orderData.id);
@@ -111,3 +116,207 @@ async function generateAccessToken() {
   const data = await response.json();
   return data.access_token;
 }
+
+async function getCartItems() {
+  const cookieStore = cookies();
+  const deviceIdentifier = cookieStore.get("device_identifier")?.value ?? "";
+
+  const cart = await getCart(deviceIdentifier);
+
+  const items = cart?.items || [];
+  const productItems = items.filter((item) => item.type === "product");
+  const upsellItems = items.filter((item) => item.type === "upsell");
+
+  const productIds = productItems.length
+    ? productItems.map((product) => product.baseProductId).filter(Boolean)
+    : [];
+
+  const [baseProducts, cartUpsells] = await Promise.all([
+    getBaseProducts(productIds),
+    getCartUpsells(upsellItems),
+  ]);
+
+  const cartProducts = mapCartProductsToBaseProducts(
+    productItems,
+    baseProducts
+  );
+
+  const sortedCartItems = [...cartProducts, ...cartUpsells].sort(
+    (a, b) => b.index - a.index
+  );
+
+  return sortedCartItems;
+}
+
+const getBaseProducts = async (productIds: string[]) =>
+  getProductsByIds({
+    ids: productIds,
+    fields: ["id", "name", "slug", "pricing", "images", "options"],
+    visibility: "PUBLISHED",
+  }) as Promise<ProductType[]>;
+
+const mapCartProductsToBaseProducts = (
+  cartProducts: Array<{
+    index: number;
+    type: "product";
+    variantId: string;
+    baseProductId: string;
+    size: string;
+    color: string;
+  }>,
+  baseProducts: ProductType[]
+) =>
+  cartProducts
+    .map((cartProduct) => {
+      const baseProduct = baseProducts.find(
+        (product) => product.id === cartProduct.baseProductId
+      );
+
+      if (!baseProduct) return null;
+
+      const colorImage = baseProduct.options?.colors.find(
+        (colorOption) => colorOption.name === cartProduct.color
+      )?.image;
+
+      return {
+        baseProductId: baseProduct.id,
+        name: baseProduct.name,
+        slug: baseProduct.slug,
+        pricing: baseProduct.pricing,
+        mainImage: colorImage || baseProduct.images.main,
+        variantId: cartProduct.variantId,
+        size: cartProduct.size,
+        color: cartProduct.color,
+        index: cartProduct.index || 0,
+        type: cartProduct.type,
+      };
+    })
+    .filter(
+      (product): product is NonNullable<typeof product> => product !== null
+    );
+
+const getCartUpsells = async (
+  upsellItems: Array<{
+    type: "upsell";
+    index: number;
+    baseUpsellId: string;
+    variantId: string;
+    products: Array<{
+      id: string;
+      size: string;
+      color: string;
+    }>;
+  }>
+) => {
+  const upsellPromises = upsellItems.map(async (upsell) => {
+    const upsellData = (await getUpsell({
+      id: upsell.baseUpsellId,
+    })) as UpsellType;
+
+    if (!upsellData || !upsellData.products) {
+      return null;
+    }
+
+    const detailedProducts = upsell.products
+      .map((selectedProduct) => {
+        const baseProduct = upsellData.products.find(
+          (product) => product.id === selectedProduct.id
+        );
+
+        if (!baseProduct) return null;
+
+        const colorImage = baseProduct.options?.colors.find(
+          (colorOption) => colorOption.name === selectedProduct.color
+        )?.image;
+
+        return {
+          index: baseProduct.index,
+          id: baseProduct.id,
+          slug: baseProduct.slug,
+          name: baseProduct.name,
+          mainImage: colorImage || baseProduct.images.main,
+          basePrice: baseProduct.basePrice,
+          size: selectedProduct.size,
+          color: selectedProduct.color,
+        };
+      })
+      .filter(
+        (product): product is NonNullable<typeof product> => product !== null
+      );
+
+    if (detailedProducts.length === 0) {
+      return null;
+    }
+
+    return {
+      baseUpsellId: upsell.baseUpsellId,
+      variantId: upsell.variantId,
+      index: upsell.index,
+      type: upsell.type,
+      mainImage: upsellData.mainImage,
+      pricing: upsellData.pricing,
+      products: detailedProducts,
+    };
+  });
+
+  const results = await Promise.all(upsellPromises);
+  return results.filter(
+    (result): result is NonNullable<typeof result> => result !== null
+  );
+};
+
+const getUpsell = async ({
+  id,
+}: {
+  id: string;
+}): Promise<Partial<UpsellType> | null> => {
+  const documentRef = doc(database, "upsells", id);
+  const snapshot = await getDoc(documentRef);
+
+  if (!snapshot.exists()) {
+    return null;
+  }
+
+  const data = snapshot.data();
+
+  const productIds = data.products
+    ? data.products.map((p: { id: string }) => p.id)
+    : [];
+
+  const products: ProductType[] | null =
+    productIds.length > 0
+      ? await getProductsByIds({
+          ids: productIds,
+          fields: ["options", "images"],
+          visibility: "PUBLISHED",
+        })
+      : null;
+
+  if (!products || products.length === 0) {
+    return null;
+  }
+
+  const updatedProducts = data.products
+    .map((product: any) => {
+      const matchedProduct = products.find((p) => p.id === product.id);
+      return matchedProduct
+        ? {
+            ...product,
+            options: matchedProduct.options ?? [],
+          }
+        : null;
+    })
+    .filter((product: any) => product !== null);
+
+  const sortedProducts = updatedProducts.sort(
+    (a: any, b: any) => a.index - b.index
+  );
+
+  const upsell: Partial<UpsellType> = {
+    id: snapshot.id,
+    ...data,
+    products: sortedProducts,
+  };
+
+  return upsell;
+};
