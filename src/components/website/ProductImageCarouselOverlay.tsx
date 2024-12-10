86bf16f7bf0712f6b6938c64c77606c6ed64dce3
khanofemperia <103168530+khanofemperia@users.noreply.github.com>
2024-12-10 05:35:02 +0000

    commit

--- src/components/website/ProductImageCarouselOverlay.tsx
+++ src/components/website/ProductImageCarouselOverlay.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useMemo } from "react";
+import React, { useMemo } from "react";
 import Image from "next/image";
 import { CloseIconThin } from "@/icons";
-import clsx from "clsx";
 
 export function ProductImageCarouselOverlay({
   product,
@@ -21,55 +20,28 @@ export function ProductImageCarouselOverlay({
       }));
 
     return [...primaryImages, ...colorImages];
-  }, [
-    product.images.main,
-    product.images.gallery,
-    product.options.colors,
-    product.name,
-  ]);
-
-  const [currentImage, setCurrentImage] = useState<ProductImage>(allImages[0]);
-
-  const handleImageSelect = (image: ProductImage) => {
-    setCurrentImage(image);
-  };
+  }, [product.images.main, product.images.gallery, product.options.colors]);
 
   return (
-    <div className="custom-scrollbar flex justify-center py-20 w-screen h-screen overflow-x-hidden overflow-y-visible z-40 fixed top-0 bottom-0 left-0 right-0 bg-black/15">
-      <div className="max-h-[612px] w-max max-w-[1024px] p-8 relative overflow-hidden shadow rounded-2xl bg-white">
-        <div className="h-full flex gap-5">
-          <div className="h-full aspect-square items-center justify-center rounded-3xl overflow-hidden">
-            <Image
-              src={currentImage.src}
-              alt={currentImage.alt}
-              width={544}
-              height={544}
-              priority
-              loading="eager"
-            />
+    <div className="custom-scrollbar flex justify-center py-20 w-screen h-screen overflow-x-hidden overflow-y-auto z-30 fixed top-0 left-0 bg-black/15">
+      <div className="w-[calc(100%-36px)] max-w-[580px] max-h-[764px] relative overflow-hidden rounded-2xl shadow bg-white">
+        <div className="h-full pt-5 pb-8 flex flex-col relative">
+          <div className="pb-3 px-5">
+            <p className="font-semibold text-center">{product.name}</p>
           </div>
-          <div className="h-full w-[320px]">
-            <p className="font-semibold mb-5">{product.name}</p>
-            <div className="flex gap-2 flex-wrap">
-              {allImages.map((image, index) => (
+          <div className="pl-5 pr-2 custom-scrollbar overflow-x-hidden overflow-y-auto">
+            <div className="flex gap-2 flex-col">
+              {allImages.map((image) => (
                 <div
-                  key={index}
-                  className={clsx(
-                    "w-[74px] h-[74px] overflow-hidden rounded-md cursor-pointer",
-                    {
-                      "ring-1 ring-blue ring-offset-2":
-                        currentImage.src === image.src,
-                    }
-                  )}
-                  onClick={() => handleImageSelect(image)}
-                  onMouseEnter={() => handleImageSelect(image)}
+                  key={image.src}
+                  className="w-full max-w-[580px] aspect-square overflow-hidden flex items-center justify-center mx-auto"
                 >
                   <Image
                     src={image.src}
                     alt={image.alt}
-                    width={74}
-                    height={74}
-                    loading="eager"
+                    width={580}
+                    height={580}
+                    priority={true}
                   />
                 </div>
               ))}
@@ -79,7 +51,8 @@ export function ProductImageCarouselOverlay({
         <button
           type="button"
           onClick={onClose}
-          className="w-9 h-9 rounded-full absolute top-[6px] right-[6px] flex items-center justify-center ease-in-out transition duration-300 hover:bg-lightgray"
+          className="w-9 h-9 rounded-full absolute top-[6px] right-[6px] flex items-center justify-center transition hover:bg-lightgray"
+          aria-label="Close carousel"
         >
           <CloseIconThin size={24} className="stroke-gray" />
         </button>
