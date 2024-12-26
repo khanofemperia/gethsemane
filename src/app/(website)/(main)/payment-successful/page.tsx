e53a20b03cbb2814f58d27099224ff1da3a935e2
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2024-12-26 23:39:21 +0000

    commit

--- src/app/(website)/(main)/payment-successful/page.tsx
+++ src/app/(website)/(main)/payment-successful/page.tsx
@@ -34,7 +34,7 @@ export default async function PaymentSuccessful({
         </div>
         <Link
           href="/"
-          className="mt-12 mx-auto w-max px-8 flex items-center justify-center rounded-full cursor-pointer border border-[#c5c3c0] text-sm font-semibold h-[44px] shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#faf9f8_5%,_#eae8e6_100%)] bg-[#faf9f8] hover:[background:linear-gradient(to_bottom,_#eae8e6_5%,_#faf9f8_100%)] hover:bg-[#eae8e6] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.14)] min-[896px]:text-base min-[896px]:h-12"
+          className="mt-11 mx-auto w-max px-8 flex items-center justify-center rounded-full cursor-pointer border border-[#c5c3c0] text-sm font-semibold h-[44px] shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#faf9f8_5%,_#eae8e6_100%)] bg-[#faf9f8] hover:[background:linear-gradient(to_bottom,_#eae8e6_5%,_#faf9f8_100%)] hover:bg-[#eae8e6] active:shadow-[inset_0_3px_8px_rgba(0,0,0,0.14)] min-[896px]:text-base min-[896px]:h-12"
         >
           Keep shopping
         </Link>
