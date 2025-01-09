825065bd2aafe08d7dc41fa424ce08ad369be87b
bundlestandoperator <160467976+bundlestandoperator@users.noreply.github.com>
2025-01-09 08:07:42 +0000

    commit

--- src/components/shared/IDCopyButton.tsx
+++ src/components/shared/IDCopyButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { TbCopy } from "react-icons/tb";
+import { Copy } from "lucide-react";
 
 export default function IDCopyButton({ id }: { id: string }) {
   const [isCopied, setIsCopied] = useState<boolean>(false);
@@ -31,11 +31,11 @@ export default function IDCopyButton({ id }: { id: string }) {
 
   return (
     <button
-      className="flex gap-2 items-center justify-between px-4 w-max h-9 rounded-full cursor-pointer shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#f9f9f9_5%,_#e9e9e9_100%)] bg-[#f9f9f9] border border-[#dcdcdc] text-[#666666] hover:[background:linear-gradient(to_bottom,_#e9e9e9_5%,_#f9f9f9_100%)] hover:bg-[#e9e9e9]"
+      className="flex gap-2 items-center justify-between px-4 w-max h-9 rounded-full cursor-pointer shadow-[inset_0px_1px_0px_0px_#ffffff] [background:linear-gradient(to_bottom,_#f9f9f9_5%,_#e9e9e9_100%)] bg-[#f9f9f9] border border-[#dcdcdc] text-[#666666] hover:[background:linear-gradient(to_bottom,_#e9e9e9_5%,_#f9f9f9_100%)] active:bg-[#e9e9e9] lg:hover:bg-[#e9e9e9]"
       type="button"
       onClick={() => handleCopyId(id)}
     >
-      <TbCopy className="text-gray" size={18} />
+      <Copy color="#6c6c6c" size={16} strokeWidth={2} />
       <span className="text-sm font-semibold text-gray">
         {isCopied ? "ID Copied!" : "Copy ID to Clipboard"}
       </span>
